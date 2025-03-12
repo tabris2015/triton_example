c1529c577b26e6fd7156f13e126c619422c36908
Jose Laruta <eduardo.laruta@gmail.com>
2025-03-12 19:24:51 -0400

    test dynamic batching

--- utils/pt2onnx.py
+++ utils/pt2onnx.py
@@ -1,7 +1,7 @@
 import torch
 from utils.model import STRModel
 
-def export_to_onnx(model_path: str, onnx_path: str):
+def export_to_onnx(model_path: str, onnx_path: str, dynamic_batching: bool = False):
     # create Pytorch Model Object
     model = STRModel(input_channels=1, output_channels=512, num_classes=37)
 
@@ -13,8 +13,10 @@ def export_to_onnx(model_path: str, onnx_path: str):
 
     # create ONNX file by tracing model
     trace_input = torch.randn(1, 1, 32, 100)
-
-    torch.onnx.export(model, trace_input, onnx_path, verbose=True)
+    if dynamic_batching:
+        torch.onnx.export(model, trace_input, onnx_path, verbose=True, dynamic_axes={"input.1": [0], "308": [0]})
+    else:
+        torch.onnx.export(model, trace_input, onnx_path, verbose=True)
 
 
 if __name__ == "__main__":
@@ -23,8 +25,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Export PyTorch model to ONNX")
     parser.add_argument("--model-path", type=str, required=True, help="Path to the PyTorch model file (.pth)")
     parser.add_argument("--onnx-path", type=str, required=True, help="Path where the ONNX model will be saved")
+    parser.add_argument("--dynamic-batching", action="store_true", help="Enable dynamic batching")
 
     args = parser.parse_args()
-    export_to_onnx(args.model_path, args.onnx_path)
+    export_to_onnx(args.model_path, args.onnx_path, args.dynamic_batching)
     print(f"ONNX model saved to {args.onnx_path}")
     
